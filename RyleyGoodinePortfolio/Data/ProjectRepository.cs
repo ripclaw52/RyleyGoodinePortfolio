38ab7ed6d40a92b946e6bdd9619f04e35353e010
Ryley Goodine <ryleygoodine96@gmail.com>
2026-05-04 15:40:57 -0600

    Add modal media gallery and improve media card UI
    Refactored MediaProjectCard to open a modal gallery on click, with distinct handling for images and video thumbnails. Added MediaGalleryModal component for viewing and navigating media in a modal with keyboard support. Updated ProjectDetails to use the new card and modal, and improved the media grid layout. Updated a project video URL to use Vimeo.

--- RyleyGoodinePortfolio/Data/ProjectRepository.cs
+++ RyleyGoodinePortfolio/Data/ProjectRepository.cs
@@ -120,7 +120,7 @@ namespace RyleyGoodinePortfolio.Data
                         new()
                         {
                             Type=MediaType.Video,
-                            Url="https://geo.dailymotion.com/player.html?video=xa6wkte",
+                            Url="https://player.vimeo.com/video/1189181438?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
                             Caption="Life and Death of a Star",
                             Technologies = new()
                             {
